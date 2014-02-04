8c3a31bfe3e96040644c6a2b046b12f57a4bf54b
Kean H Lau <khklau@users.noreply.github.com>
2014-02-05 10:22:27 +1100

    Initial check in of Protocol Buffers based Grid DB schema

---
+++ main/grid_db/include/header.hpp
@@ -0,0 +1,16 @@
+#ifndef SIMULATION_GRID_GRID_DB_HEADER_HPP
+#define SIMULATION_GRID_GRID_DB_HEADER_HPP
+
+#include <boost/noncopyable.hpp>
+
+namespace simulation_grid {
+namespace grid_db {
+
+class header : private boost::noncopyable
+{
+};
+
+} // namespace grid_db
+} // namespace simulation_grid
+
+#endif
