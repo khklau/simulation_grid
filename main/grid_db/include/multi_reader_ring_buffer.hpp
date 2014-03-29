36854466acf566c81c53ff308cdc7383740fd859
Kean H Lau <khklau@users.noreply.github.com>
2014-03-30 00:58:24 +1100

    updated multi_reader_ring_buffer to now be storable inside boost::interprocess:managed_mapped_file

--- main/grid_db/include/multi_reader_ring_buffer.hpp
+++ main/grid_db/include/multi_reader_ring_buffer.hpp
@@ -1,25 +1,29 @@
-#ifndef SIMULATION_GRID_GRID_DB_MULTI_READER_RING_BUFFER_HPP
-#define SIMULATION_GRID_GRID_DB_MULTI_READER_RING_BUFFER_HPP
+#ifndef SIMULATION_GRID_GRID_DB_MULTI_READER_RING_RINGBUF_HPP
+#define SIMULATION_GRID_GRID_DB_MULTI_READER_RING_RINGBUF_HPP
 
 #include <memory>
 #include <boost/noncopyable.hpp>
-#include <boost/thread/locks.hpp>
-#include <boost/thread/shared_mutex.hpp>
+#include <boost/interprocess/interprocess_fwd.hpp>
+#include <boost/interprocess/allocators/allocator.hpp>
+#include <boost/interprocess/smart_ptr/deleter.hpp>
+#include <boost/interprocess/smart_ptr/scoped_ptr.hpp>
+#include <boost/interprocess/sync/interprocess_sharable_mutex.hpp>
 #include <boost/circular_buffer.hpp>
 
 namespace simulation_grid {
 namespace grid_db {
 
-template <class element_t, class allocator_t = std::allocator<element_t> >
+template <class element_t, class memory_t>
 class multi_reader_ring_buffer : private boost::noncopyable
 {
 public:
+    typedef boost::interprocess::allocator<element_t, typename memory_t::segment_manager> allocator_t;
     typedef typename allocator_t::reference element_ref_t;
     typedef typename allocator_t::const_reference const_element_ref_t;
     typedef typename allocator_t::size_type size_type;
-    typedef boost::shared_lock<boost::shared_mutex> read_lock;
-    typedef boost::unique_lock<boost::shared_mutex> write_lock;
-    multi_reader_ring_buffer(size_type capacity);
+    typedef boost::interprocess::sharable_lock<boost::interprocess::interprocess_sharable_mutex> read_lock;
+    typedef boost::interprocess::scoped_lock<boost::interprocess::interprocess_sharable_mutex> write_lock;
+    multi_reader_ring_buffer(size_type capacity, memory_t* file);
     const_element_ref_t front() const;
     void push_front(const_element_ref_t element);
     const_element_ref_t back() const;
@@ -29,8 +33,22 @@ public:
     bool empty() const;
     bool full() const;
 private:
-    boost::shared_mutex mutex_;
-    boost::circular_buffer<element_t, allocator_t> buffer_;
+    typedef boost::interprocess::deleter<boost::interprocess::interprocess_sharable_mutex, typename memory_t::segment_manager>  mutex_deleter_t;
+    typedef boost::interprocess::deleter<boost::circular_buffer<element_t, allocator_t>, typename memory_t::segment_manager>  ringbuf_deleter_t;
+    //boost::interprocess::interprocess_sharable_mutex mutex_;
+    //boost::circular_buffer<element_t, allocator_t> buffer_;
+    allocator_t allocator_;
+    mutex_deleter_t mdeleter_;
+    ringbuf_deleter_t rdeleter_;
+    boost::interprocess::scoped_ptr<boost::interprocess::interprocess_sharable_mutex, mutex_deleter_t> mutex_;
+    boost::interprocess::scoped_ptr<boost::circular_buffer<element_t, allocator_t>, ringbuf_deleter_t> ringbuf_;
+};
+
+// TODO : replace the following with type aliases after moving to a C++11 compiler
+template <class element_t>
+struct mmap_multi_reader_ring_buffer
+{
+    typedef multi_reader_ring_buffer<element_t, boost::interprocess::managed_mapped_file> type;
 };
 
 } // namespace grid_db
--- main/grid_db/include/multi_reader_ring_buffer.hxx
+++ main/grid_db/include/multi_reader_ring_buffer.hxx
@@ -1,75 +1,93 @@
 #ifndef SIMULATION_GRID_GRID_DB_MULTI_READER_RING_BUFFER_HXX
 #define SIMULATION_GRID_GRID_DB_MULTI_READER_RING_BUFFER_HXX
 
+#include <boost/circular_buffer.hpp>
+#include <boost/interprocess/managed_mapped_file.hpp>
+#include <boost/interprocess/sync/scoped_lock.hpp>
+#include <boost/interprocess/sync/sharable_lock.hpp>
 #include "multi_reader_ring_buffer.hpp"
+
+namespace bi = boost::interprocess;
+
+namespace {
+
+static const char* MUTEX_KEY_ = "@@MUTEX@@";
+static const char* RINGBUF_KEY_ = "@@RING_BUFFER@@";
+
+} // annonymous namespace
 
 namespace simulation_grid {
 namespace grid_db {
 
-template <class element_t, class allocator_t>
-multi_reader_ring_buffer<element_t, allocator_t>::multi_reader_ring_buffer(size_type capacity)
+template <class element_t, class memory_t>
+multi_reader_ring_buffer<element_t, memory_t>::multi_reader_ring_buffer(size_type capacity, memory_t* file) :
+    allocator_(file->get_segment_manager()),
+    mdeleter_(file->get_segment_manager()),
+    rdeleter_(file->get_segment_manager()),
+    mutex_(file->template construct<bi::interprocess_sharable_mutex>(MUTEX_KEY_)(), mdeleter_),
+    ringbuf_(file->template construct<boost::circular_buffer<element_t, allocator_t> >(RINGBUF_KEY_)(capacity, allocator_), rdeleter_)
 {
-    buffer_.set_capacity(capacity);
+    ringbuf_->set_capacity(capacity);
 }
 
-template <class element_t, class allocator_t>
-typename multi_reader_ring_buffer<element_t, allocator_t>::const_element_ref_t multi_reader_ring_buffer<element_t, allocator_t>::front() const
+template <class element_t, class memory_t>
+typename multi_reader_ring_buffer<element_t, memory_t>::const_element_ref_t multi_reader_ring_buffer<element_t, memory_t>::front() const
 {
-    read_lock(mutex_);
-    return buffer_.front();
+    read_lock(*(this->mutex_));
+    return ringbuf_->front();
 }
 
-template <class element_t, class allocator_t>
-void multi_reader_ring_buffer<element_t, allocator_t>::push_front(const_element_ref_t value)
+template <class element_t, class memory_t>
+void multi_reader_ring_buffer<element_t, memory_t>::push_front(const_element_ref_t value)
 {
-    write_lock(mutex_);
-    buffer_.push_front(value);
+    write_lock(*(this->mutex_));
+    ringbuf_->push_front(value);
 }
 
-template <class element_t, class allocator_t>
-typename multi_reader_ring_buffer<element_t, allocator_t>::const_element_ref_t multi_reader_ring_buffer<element_t, allocator_t>::back() const
+template <class element_t, class memory_t>
+typename multi_reader_ring_buffer<element_t, memory_t>::const_element_ref_t multi_reader_ring_buffer<element_t, memory_t>::back() const
 {
-    read_lock(mutex_);
-    return buffer_.back();
+    read_lock(*(this->mutex_));
+    return ringbuf_->back();
 }
 
-template <class element_t, class allocator_t>
-void multi_reader_ring_buffer<element_t, allocator_t>::pop_back(const_element_ref_t back_element)
+template <class element_t, class memory_t>
+void multi_reader_ring_buffer<element_t, memory_t>::pop_back(const_element_ref_t back_element)
 {
-    write_lock(mutex_);
+    write_lock(*(this->mutex_));
     const_element_ref_t current_back = back();
     if (&back_element == &current_back)
     {
-	buffer_.pop_back();
+	ringbuf_->pop_back();
     }
 }
 
-template <class element_t, class allocator_t>
-typename multi_reader_ring_buffer<element_t, allocator_t>::size_type multi_reader_ring_buffer<element_t, allocator_t>::capacity() const
+template <class element_t, class memory_t>
+typename multi_reader_ring_buffer<element_t, memory_t>::size_type multi_reader_ring_buffer<element_t, memory_t>::capacity() const
 {
-    read_lock(mutex_);
-    return buffer_.capacity();
+    read_lock(*(this->mutex_));
+    return ringbuf_->capacity();
 }
 
-template <class element_t, class allocator_t>
-typename multi_reader_ring_buffer<element_t, allocator_t>::size_type multi_reader_ring_buffer<element_t, allocator_t>::element_count() const
+template <class element_t, class memory_t>
+typename multi_reader_ring_buffer<element_t, memory_t>::size_type multi_reader_ring_buffer<element_t, memory_t>::element_count() const
 {
-    read_lock(mutex_);
-    return buffer_.size();
+    read_lock(*(this->mutex_));
+    return ringbuf_->size();
 }
 
-template <class element_t, class allocator_t>
-bool multi_reader_ring_buffer<element_t, allocator_t>::empty() const
+template <class element_t, class memory_t>
+bool multi_reader_ring_buffer<element_t, memory_t>::empty() const
 {
-    read_lock(mutex_);
-    return buffer_.empty();
+    read_lock(*(this->mutex_));
+    return ringbuf_->empty();
 }
 
-template <class element_t, class allocator_t>
-bool multi_reader_ring_buffer<element_t, allocator_t>::full() const
+template <class element_t, class memory_t>
+bool multi_reader_ring_buffer<element_t, memory_t>::full() const
 {
-    read_lock(mutex_);
-    return buffer_.full();
+    read_lock(*(this->mutex_));
+    return ringbuf_->full();
 }
 
 } // namespace grid_db
