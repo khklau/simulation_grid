1b6a753305c8e9d09b7db9fb58dc31e1ed779d50
Kean H Lau <khklau@users.noreply.github.com>
2014-03-31 18:21:27 +1100

    naming via RINGBUF_KEY_ doesn't work because we end up with a name clash when multiple multi_reader_ring_buffer instances are instantiated; it's unnecessary anyway because we can construct it anonymously since the resulting pointer is handled by scoped_ptr and destroyed correctly via the deleter

--- main/grid_db/include/multi_reader_ring_buffer.hxx
+++ main/grid_db/include/multi_reader_ring_buffer.hxx
@@ -10,12 +10,6 @@
 
 namespace bi = boost::interprocess;
 
-namespace {
-
-static const char* RINGBUF_KEY_ = "@@RING_BUFFER@@";
-
-} // annonymous namespace
-
 namespace simulation_grid {
 namespace grid_db {
 
@@ -23,7 +17,7 @@ template <class element_t, class memory_t>
 multi_reader_ring_buffer<element_t, memory_t>::multi_reader_ring_buffer(size_type capacity, memory_t* file) :
     allocator_(file->get_segment_manager()),
     deleter_(file->get_segment_manager()),
-    ringbuf_(file->template construct<boost::circular_buffer<element_t, allocator_t> >(RINGBUF_KEY_)(capacity, allocator_), deleter_)
+    ringbuf_(file->template construct<boost::circular_buffer<element_t, allocator_t> >(bi::anonymous_instance)(capacity, allocator_), deleter_)
 {
     ringbuf_->set_capacity(capacity);
 }
